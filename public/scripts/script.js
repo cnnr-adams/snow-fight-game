5075c692056228efd50c3176722dd079c45d608a
Vanilla Rain <cnnr252@gmail.com>
2018-09-14 17:53:19 -0400

    Semi-functioning

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -38,8 +38,8 @@ function preload() {
     this.load.image('playercollision', 'resources/playercollision.png');
 }
 var lightAngle = Math.PI / 4;
-var numberOfRays = 20;
-var rayLength = 50;
+var numberOfRays = 50;
+var rayLength = 100;
 var otherPlayers = new Map();
 function create() {
     console.log("CREATE!");
@@ -70,6 +70,7 @@ function create() {
         }
     });
 }
+
 var timeSinceUpdate = 0;
 function update(time, delta) {
     if (cursors.up.isDown) {
@@ -107,27 +108,98 @@ function update(time, delta) {
         var points = [player.x, player.y];
         var mouseAngle = Math.atan2(player.y - (game.input.mousePointer.y + this.cameras.main.scrollY), player.x - (game.input.mousePointer.x + this.cameras.main.scrollX));
         for (var i = 0; i < numberOfRays; i++) {
-            var rayAngle = mouseAngle - (lightAngle / 2) + (lightAngle / numberOfRays) * i
-            var lastX = player.x;
-            var lastY = player.y;
-            for (var j = 1; j <= rayLength; j += 1) {
-                var landingX = player.x - (2 * j) * Math.cos(rayAngle);
-                var landingY = player.y - (2 * j) * Math.sin(rayAngle);
-                if (!wallSet.has(`${Math.round(landingX / 16)}-${Math.round(landingY / 16)}`)) {
-                    lastX = landingX;
-                    lastY = landingY;
+            var ray = (180 - toDegrees(mouseAngle - (lightAngle / 2) + (lightAngle / numberOfRays) * i));
+            var rayAngle = ray < 0 ? 360 + ray : ray;
+            const yRatio = Math.tan(toRadians(rayAngle));
+            const xRatio = 1 / yRatio;
+            let currentX = player.x;
+            let currentY = player.y;
+            let currentDistance = 0;
+            while (currentDistance < rayLength) {
+                let nearestXWall;
+                let nearestYWall;
+                if (rayAngle >= 0 && rayAngle < 90) {
+                    //  console.log("0", currentX, Math.ceil(currentX / 16) * 16);
+                    //x to right, y up
+                    nearestXWall = Math.ceil(currentX / 16) * 16;
+                    nearestYWall = Math.floor(currentY / 16) * 16;
+                    nearestXWall += nearestXWall === currentX ? 16 : 0;
+                    nearestYWall -= nearestYWall === currentY ? 16 : 0;
+                } else if (rayAngle >= 90 && rayAngle < 180) {
+                    //     console.log("90", currentX, Math.floor(currentX / 16) * 16);
+                    //x to left, y up
+                    nearestXWall = Math.floor(currentX / 16) * 16;
+                    nearestYWall = Math.floor(currentY / 16) * 16;
+                    nearestXWall -= nearestXWall === currentX ? 16 : 0;
+                    nearestYWall -= nearestYWall === currentY ? 16 : 0;
+                } else if (rayAngle >= 180 && rayAngle < 270) {
+                    // console.log("180", currentX, Math.floor(currentX / 16) * 16);
+                    //x to left, y down
+                    nearestXWall = Math.floor(currentX / 16) * 16;
+                    nearestYWall = Math.ceil(currentY / 16) * 16;
+                    nearestXWall -= nearestXWall === currentX ? 16 : 0;
+                    nearestYWall += nearestYWall === currentY ? 16 : 0;
+                } else {
+                    // console.log("270", currentX, Math.ceil(currentX / 16) * 16);
+                    //x to right, y down
+                    nearestXWall = Math.ceil(currentX / 16) * 16;
+                    nearestYWall = Math.ceil(currentY / 16) * 16;
+                    nearestXWall += nearestXWall === currentX ? 16 : 0;
+                    nearestYWall += nearestYWall === currentY ? 16 : 0;
                 }
-                else {
-                    //  console.log(lastX, lastY);
-                    points.push(lastX, lastY);
-                    break;
+
+                const xDistance = Math.abs(currentX - nearestXWall);
+                const yDistance = Math.abs(currentY - nearestYWall);
+
+                // Essentially calculates inverse time each would take to REACH ((((((((I think))))))))
+                if ((xRatio / xDistance) >= (yRatio / yDistance)) {
+                    currentX = nearestXWall;
+                    currentY += xDistance * yRatio;
+                } else {
+                    currentY = nearestYWall;
+                    currentX += yDistance * xRatio;
+                }
+                // console.log(xDistance, yDistance, currentX, player.x, currentY, player.y)
+                currentDistance = Math.sqrt(Math.pow(currentX - player.x, 2) + Math.pow(currentY - player.y, 2));
+                if (currentDistance > rayLength) {
+                    //hypot: raylength, close = hcos(theta), far = hsin(theta)
+                    currentX = rayLength * toDegrees(Math.cos(toRadians(rayAngle)));
+                    currentY = rayLength * toDegrees(Math.sin(toRadians(rayAngle)));
                 }
             }
-            points.push(lastX, lastY);
+            points.push(currentX, currentY);
+
+            /* var lastX = player.x;
+             var lastY = player.y;
+             for (var j = 1; j <= rayLength; j += 0.1) {
+                 var landingX = player.x - (j) * Math.cos(rayAngle);
+                 var landingY = player.y - (j) * Math.sin(rayAngle);
+                 if (!wallSet.has(`${Math.round(landingX / 16)}-${Math.round(landingY / 16)}`)) {
+                     lastX = landingX;
+                     lastY = landingY;
+                 }
+                 else {
+                     //  console.log(lastX, lastY);
+                     points.push(lastX, lastY);
+                     break;
+                 }
+             }*/
+            // points.push(lastX, lastY);
         }
+        // maskGraphics.fillStyle(2, 0xffffff);
+        // var circle = new Phaser.Geom.Circle(player.x, player.y, 20);
+        // maskGraphics.fillCircleShape(circle, true);
         var polygon = new Phaser.Geom.Polygon(points);
+        maskGraphics.fillStyle(0xffffe0);
         maskGraphics.fillPoints(polygon.points, true);
+
     }
+}
+function toDegrees(angle) {
+    return angle * (180 / Math.PI);
+}
+function toRadians(angle) {
+    return angle * (Math.PI / 180);
 }
 
 var maskGraphics;
